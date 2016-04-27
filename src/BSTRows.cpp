61feec30ff7db8ad9b8b6e6f1fc7e4736dbf3c71
dinesh1729v <dinesh1729v@missionrnd.com>
2016-04-28 01:07:59 +0530

    Submit Commit

--- src/BSTRows.cpp
+++ src/BSTRows.cpp
@@ -22,16 +22,96 @@ Note : Return -1 for Invalid Cases .
 
 #include <stdlib.h>
 #include <stdio.h>
-
 struct node{
 	struct node * left;
 	int data;
 	struct node *right;
 };
-
-
-
+int max1(int a, int b)
+{
+	return (a > b ? a : b);
+}
+int get_height1(struct node *root){
+	if (root == NULL)return 0;
+	int leftHeight = get_height1(root->left);
+	int rightHeight = get_height1(root->right);
+	return 1 + max1(leftHeight, rightHeight);
+}
+int getNoOfNodes(struct node * root)
+{
+	if (root == NULL)return 0;
+	int left = getNoOfNodes(root->left);
+	int right = getNoOfNodes(root->right);
+	return 1 + left + right;
+}
+void addToArray(struct node* root, int * arr, int * index, int level)
+{
+	if (root == NULL)return;
+	if (level == 0)
+	{
+		arr[*index] = root->data;
+		*index = *index + 1;
+	}
+	else
+	{
+		addToArray(root->right, arr, index, level - 1);
+		addToArray(root->left, arr, index, level - 1);
+	}
+}
 int* BSTRighttoLeftRows(struct node* root)
 {
-    return NULL;
+	if (root == NULL)return NULL;
+	int height = get_height1(root),cnt=getNoOfNodes(root);
+	int *arr=(int *)malloc(sizeof(int)*cnt),i,j,index=0,rear=0,front=0;
+	for (i = 0; i < height; i++)
+	{
+		addToArray(root, arr, &index,i);
+	}
+	return arr;
 }
+/*
+
+int max1(int a, int b)
+{
+	return (a > b ? a : b);
+}
+int get_height1(struct node *root){
+	if (root == NULL)return 0;
+	int leftHeight = get_height1(root->left);
+	int rightHeight = get_height1(root->right);
+	return 1 + max1(leftHeight, rightHeight);
+}
+int getNoOfNodes(struct node * root)
+{
+	if (root == NULL)return 0;
+	int left = getNoOfNodes(root->left);
+	int right = getNoOfNodes(root->right);
+	return 1 + left + right;
+}
+int* BSTRighttoLeftRows(struct node* root)
+{
+	if (root == NULL)return NULL;
+	int height = get_height1(root),cnt=getNoOfNodes(root);
+	int *arr=(int *)malloc(sizeof(int)*cnt),i,j,index=0,rear=0,front=0;
+	struct node **queue = (struct node **)calloc((cnt/2),sizeof(struct node *)),*temp=root;
+	for (i = 0; i < cnt; i++)
+		queue[i] = (struct node *)calloc(1,sizeof(struct node));
+	while (index<cnt)
+	{
+		arr[index++] = temp->data;
+		if (temp->right!=NULL)
+		{
+			rear++;
+			queue[rear] = temp->right;
+		}
+		if (temp->left != NULL)
+		{
+			rear++;
+			queue[rear] = temp->left;
+		}
+		temp = queue[++front];
+	}	
+	return arr;
+}
+
+*/
